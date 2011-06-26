9343e49bb686e8c6305ecc781a5b28cccb557698
high <pooop828@hotmail.com>
2011-06-25 22:50:35 -0400

    Base project added. Please read the GUIDELINES.

---
+++ TShock/TShock/TShockPlugin.cs
@@ -0,0 +1,46 @@
+﻿/*
+TShock, a server mod for Terraria
+Copyright (C) 2011 The TShock Team
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+using TerrariaAPI;
+
+namespace TShock
+{
+    public class TShockPlugin : TerrariaPlugin
+    {
+        public TShockPlugin(Main game)
+            : base(game)
+        {
+        }
+
+        public override void DeInitialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
